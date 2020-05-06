dd8c6df89c631615850ea01a9a20f021ade65b9c
Hossein Dehghan <mh.dehghan@ut.ac.ir>
2020-05-07 01:08:06 +0430

    Chane input way!

--- src/main/java/contest/P34081.java
+++ src/main/java/contest/P34081.java
@@ -2,14 +2,11 @@ package contest;
 import java.util.Scanner;
 
 public class P34081 {
-
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String s = in.nextLine();
-        String[] input = s.split(" ");
-        int n = Integer.parseInt(input[0]);
-        int k = Integer.parseInt(input[1]);
-        int start = 0;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int start = 1;
         int cnt = 0;
         int next_step;
         while (true) {
